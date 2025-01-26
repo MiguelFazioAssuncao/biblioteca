2737292228f1c10e7b2ce6de54878833a848144d
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-01-26 15:25:18 -0300

    Adicionado: - Getters e setters por meio da annotation @Data - Table livro - Id - Columns titulo, auto e anoPublicacao

--- src/main/java/com/miguelfazio/project_mini_exercise/database/entity/LivroEntity.java
+++ src/main/java/com/miguelfazio/project_mini_exercise/database/entity/LivroEntity.java
@@ -1,4 +1,23 @@
 package com.miguelfazio.project_mini_exercise.database.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "livro")
 public class LivroEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(100)", nullable = false)
+    private String titulo;
+
+    @Column(columnDefinition = "VARCHAR(80)")
+    private String autor;
+
+    @Column(columnDefinition = "INT")
+    private int anoPublicacao;
+
 }
