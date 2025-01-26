bc520a7fa4452446d14ba35f56b8b5c8605848b7
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-01-26 15:05:30 -0300

    Estrutura inicial

---
+++ src/main/java/com/miguelfazio/project_mini_exercise/ProjectMiniExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.miguelfazio.project_mini_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMiniExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMiniExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguelfazio/project_mini_exercise/ProjectMiniExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelfazio.project_mini_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMiniExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
