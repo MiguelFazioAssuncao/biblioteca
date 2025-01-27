42c8520562e231f85e86febdbb3925726820694e
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-01-27 11:45:48 -0300

    Criando caminho padrão de requisição com @RequestMapping

--- src/main/java/com/miguelfazio/project_mini_exercise/controller/LivroController.java
+++ src/main/java/com/miguelfazio/project_mini_exercise/controller/LivroController.java
@@ -2,9 +2,11 @@ package com.miguelfazio.project_mini_exercise.controller;
 
 import com.miguelfazio.project_mini_exercise.database.entity.LivroEntity;
 import com.miguelfazio.project_mini_exercise.service.LivroService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/livro")
 public class LivroController extends GenericController<LivroEntity, LivroEntity, LivroService> {
 
     public LivroController(LivroService service) {
--- src/main/java/com/miguelfazio/project_mini_exercise/controller/MembroController.java
+++ src/main/java/com/miguelfazio/project_mini_exercise/controller/MembroController.java
@@ -2,9 +2,11 @@ package com.miguelfazio.project_mini_exercise.controller;
 
 import com.miguelfazio.project_mini_exercise.database.entity.MembroEntity;
 import com.miguelfazio.project_mini_exercise.service.MembroService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/membros")
 public class MembroController extends GenericController<MembroEntity, MembroEntity, MembroService> {
 
     public MembroController(MembroService service) {
