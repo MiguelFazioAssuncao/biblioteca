d7ff6149e9e4f4603897d84e9154ead27d48d40c
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-01-27 21:22:17 -0300

    Criado BibliotecarioService com métodos para sincronizar propriedades e criar novas entidades.

---
+++ src/main/java/com/miguelfazio/project_mini_exercise/service/BibliotecarioService.java
@@ -0,0 +1,32 @@
+package com.miguelfazio.project_mini_exercise.service;
+
+import com.miguelfazio.project_mini_exercise.database.entity.BibliotecarioEntity;
+import com.miguelfazio.project_mini_exercise.database.repository.BibliotecarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BibliotecarioService extends GenericServiceImpl<BibliotecarioEntity, BibliotecarioEntity, BibliotecarioRepository> implements GenericService<BibliotecarioEntity, BibliotecarioEntity> {
+
+    public BibliotecarioService(BibliotecarioRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public BibliotecarioEntity equalProperties(BibliotecarioEntity entity, BibliotecarioEntity data) {
+        if (data.getNome() != null) {
+            entity.setNome(data.getNome());
+        }
+        if (data.getEmail() != null) {
+            entity.setEmail(data.getEmail());
+        }
+        if (data.getSenha() != null) {
+            entity.setSenha(data.getSenha());
+        }
+        return entity;
+    }
+
+    @Override
+    public BibliotecarioEntity newEntity() {
+        return new BibliotecarioEntity();
+    }
+}
