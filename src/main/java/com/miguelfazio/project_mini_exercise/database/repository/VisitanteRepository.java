285ff22182d5766f13f6fbf66d35052d9de02853
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-01-27 21:42:52 -0300

    Criado VisitanteRepository para acesso a dados da entidade VisitanteEntity

---
+++ src/main/java/com/miguelfazio/project_mini_exercise/database/repository/VisitanteRepository.java
@@ -0,0 +1,9 @@
+package com.miguelfazio.project_mini_exercise.database.repository;
+
+import com.miguelfazio.project_mini_exercise.database.entity.VisitanteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VisitanteRepository extends JpaRepository<VisitanteEntity, Long> {
+}
